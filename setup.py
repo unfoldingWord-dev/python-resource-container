10336991dbf169afb5ba40e919ab168a510ecbb6
Joel Lonbeck <joel@neutrinographics.com>
2017-05-22 15:48:48 -0700

    preparing for deployment to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), "r") as f:
+    long_description = f.read()
 
 setup(name='resource_container',
       version='0.1',
       description='A utility for managing Door43 Resource Containers',
+      long_description=long_description,
       url='https://github.com/unfoldingWord-dev/python-resource-container',
-      author='Door43',
+      author='unfoldingWord',
       author_email='joel@neutrinographics.com',
       license='MIT',
-      packages=['resource_container'],
+      classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+      ],
+      packages=find_packages(),
+      keywords=["rc", "resource container", "client"],
       install_requires=[
             'pyyaml',
             'uw_tools'
       ],
-      zip_safe=False)
+      test_suite="tests",
+      zip_safe=False
+)
